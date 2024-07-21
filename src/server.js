c2459befcf29ba0ba7c1f55a32803cc56ff371ab
OlenaPanf <elena.el@ukr.net>
2024-07-22 00:17:37 +0300

    add to step 2

--- src/server.js
+++ src/server.js
@@ -2,13 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino';
 import pinoHttp from 'pino-http';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const logger = pino({
   transport: {
     target: 'pino-pretty',
-    options: {
-      colorize: true,
-    },
+    // options: {
+    //   colorize: true,
+    // },
   },
 });
 
@@ -22,9 +25,25 @@ export function setupServer() {
   // Налаштування логгера pino
   app.use(httpLogger);
 
+  // Додати обробку JSON
+  app.use(express.json());
+
+  // Додамо обробник для GET-запиту на головний маршрут
+  //   app.get('/', (req, res) => {
+  //     res.send('Hello, world!');
+  //   });
+
   // Обробка неіснуючих роутів
   app.use((req, res, next) => {
     res.status(404).json({ message: 'Not found' });
+  });
+
+  // Обробка помилок
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
   });
 
   const PORT = process.env.PORT || 3000;
