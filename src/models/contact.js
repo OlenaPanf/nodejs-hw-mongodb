785173f97a18b815c4c8f409ccd9a51a6946c681
OlenaPanf <elena.el@ukr.net>
2024-07-23 22:18:34 +0300

    Schema

---
+++ src/models/contact.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    email: { type: String, match: /.+\@.+\..+/ },
+    isFavourite: { type: Boolean, default: false },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  { timestamps: true },
+);
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+export default Contact;
+
+// {
+//     "name": "Yulia Shevchenko",
+//     "phoneNumber": "+380000000001",
+//     "email": "oleh1@example.com",
+//     "isFavourite": false,
+//     "contactType": "personal",
+//     "createdAt": "2024-05-08T16:12:14.954151",
+//     "updatedAt": "2024-05-08T16:12:14.954158"
+//   }
